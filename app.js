c4b71887cebf65dce94bb87cefeb9764c1f431eb
Brandon Holliday <brandonholliday1@gmail.com>
2023-03-21 11:00:59 -0600

    Finished Assignment

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-// TODO: Enable strict mode
+//Exercise 1
+"use strict";
 
-// TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+//Exercise 3
+function parseToJSON(data) { //Removed duplicate parameter because of Strict Mode.
+  try{
+    return JSON.parse(data); 
+  }
+  catch(err){
+    console.log(err);
+    return null;
+  }
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
 let passData = JSON.stringify({ success: true });
 
